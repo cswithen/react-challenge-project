479bbf56639fa3ea74640c92e188e5833a60381b
cswithen <cody.swithenbank@gmail.com>
2021-10-20 20:00:09 -0700

    feat(edit-delete): edit and delete orders

---
+++ application/src/components/view-orders/editOrderForm/editOrderForm.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { SERVER_IP } from '../../../private';
+import './editOrderForm.css';
+
+const EDIT_ORDER_URL = `${SERVER_IP}/api/edit-order`
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+})
+
+class EditOrderForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            order_item: "",
+            quantity: "1"
+        }
+    }
+
+    menuItemChosen(event) {
+        this.setState({ order_item: event.target.value });
+    }
+
+    menuQuantityChosen(event) {
+        this.setState({ quantity: event.target.value });
+    }
+
+    submitOrder(event) {
+        event.preventDefault();
+        if (this.state.order_item === "") return;
+        fetch(EDIT_ORDER_URL, {
+            method: 'POST',
+            body: JSON.stringify({
+                id: this.props.editOrder.id,
+                order_item: this.state.order_item,
+                quantity: this.state.quantity,
+                ordered_by: this.props.auth.email || 'Unknown!',
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(response => console.log("Success", JSON.stringify(response)))
+        .catch(error => console.error(error));
+    }
+
+    componentDidMount() {
+      this.setState({
+            order_item: this.props.editOrder.order_item,
+            quantity: this.props.editOrder.quantity
+      })
+    }
+
+    render() {
+        return (
+                <div className="form-wrapper">
+                    <form>
+                        <label className="form-label">I'd like to order...</label><br />
+                        <select
+                            value={this.state.order_item}
+                            onChange={(event) => this.menuItemChosen(event)}
+                            className="menu-select"
+                        >
+                            <option value="" defaultValue disabled hidden>Lunch menu</option>
+                            <option value="Soup of the Day">Soup of the Day</option>
+                            <option value="Linguini With White Wine Sauce">Linguini With White Wine Sauce</option>
+                            <option value="Eggplant and Mushroom Panini">Eggplant and Mushroom Panini</option>
+                            <option value="Chili Con Carne">Chili Con Carne</option>
+                        </select><br />
+                        <label className="qty-label">Qty:</label>
+                        <select value={this.state.quantity} onChange={(event) => this.menuQuantityChosen(event)}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                        </select>
+                        <button type="button" className="order-btn" onClick={(event) => this.submitOrder(event)}>Edit Order</button>
+                    </form>
+                </div>
+        );
+    }
+}
+
+export default connect(mapStateToProps, null)(EditOrderForm);
--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
 import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
+import EditOrderForm from './editOrderForm/editOrderForm';
 import './viewOrders.css';
+
+const DELETE_ORDER_URL = `${SERVER_IP}/api/delete-order`
+
 
 class ViewOrders extends Component {
     state = {
-        orders: []
+        orders: [],
+        selectedOrder: ""
     }
 
     componentDidMount() {
-        fetch(`${SERVER_IP}/api/current-orders`)
-            .then(response => response.json())
-            .then(response => {
-                if(response.success) {
-                    this.setState({ orders: response.orders });
-                } else {
-                    console.log('Error getting orders');
-                }
-            });
+        this.fetchOrders()
     }
+
+    fetchOrders() {
+        fetch(`${SERVER_IP}/api/current-orders`)
+        .then(response => response.json())
+        .then(response => {
+            if(response.success) {
+                this.setState({ orders: response.orders });
+            } else {
+                console.log('Error getting orders');
+            }
+        });
+    }
+
+    deleteOrder(event) {
+        fetch(DELETE_ORDER_URL, {
+            method: 'POST',
+            body: JSON.stringify({
+                id: event.target.id,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(response => {console.log("Success", JSON.stringify(response))
+        })
+        .catch(error => console.error(error))
+    }
+
+    handleClick(event) {
+        if(this.state.selectedOrder === event.target.id){
+            this.setState({
+                selectedOrder: ""
+            })
+        } else {
+            this.setState({
+                selectedOrder: event.target.id
+            })
+
+        }
+    }
+
 
     render() {
         return (
@@ -26,6 +65,12 @@ class ViewOrders extends Component {
                 <div className="container-fluid">
                     {this.state.orders.map(order => {
                         const createdDate = new Date(order.createdAt);
+                        const editOrder = {
+                            id: order._id,
+                            order_item: order.order_item,
+                            quantity: order.quantity,
+                        }
+
                         return (
                             <div className="row view-order-container" key={order._id}>
                                 <div className="col-md-4 view-order-left-col p-3">
@@ -37,9 +82,11 @@ class ViewOrders extends Component {
                                     <p>Quantity: {order.quantity}</p>
                                  </div>
                                  <div className="col-md-4 view-order-right-col">
-                                     <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger">Delete</button>
+                                     <button className="btn btn-success" id={order._id} onClick={this.handleClick.bind(this)
+                                     }>Edit</button>
+                                     <button className="btn btn-danger" id={order._id} onClick={this.deleteOrder}>Delete</button>
                                  </div>
+                                 {(this.state.selectedOrder === order._id ? <EditOrderForm editOrder={editOrder}/>: <div />)}
                             </div>
                         );
                     })}
